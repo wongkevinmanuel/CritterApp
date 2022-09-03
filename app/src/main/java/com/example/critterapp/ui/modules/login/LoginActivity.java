5b56e8137d3e6edc8875f6ca3cc08f2a6f71f772
kevin <onofrekevin@gmail.com>
2022-09-03 00:10:55 -0500

    Se agrego activity LoginActivity."

---
+++ app/src/main/java/com/example/critterapp/ui/modules/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.critterapp.ui.modules.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.critterapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_form);
+    }
+}
