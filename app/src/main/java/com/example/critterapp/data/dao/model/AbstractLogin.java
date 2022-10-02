08cf92b9a0a9296ecd0a39de7d5c0f20332b60be
kevin <onofrekevin@gmail.com>
2022-10-01 21:37:37 -0500

    Se implemento ORM room, remplazando a requery.

--- app/src/main/java/com/example/critterapp/data/dao/model/AbstractLogin.java
+++ app/src/main/java/com/example/critterapp/data/dao/model/AbstractLogin.java
@@ -3,29 +3,26 @@ package com.example.critterapp.data.dao.model;
 import android.os.Parcelable;
 
 import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-import io.reactivex.Observable;
-import io.requery.Column;
-import io.requery.Entity;
-import io.requery.Key;
-import lombk.NoArgsConstructor;
-
-@Entity
-@NoArgsConstructor
+@Entity(tableName = "tb_login")
 public abstract class  AbstractLogin implements Parcelable {
-    @Key long id;
-    @Column String login;
-    String avatarUrl;
-    String name;
-    String location;
-    String email;
-    String token;
+    @PrimaryKey
+    private long id;
+    private String avatarUrl;
+    @ColumnInfo(name="user_name")
+    private String nameUser;
+    private String location;
+    private String email;
+    private String token;
 
-    @Nullable boolean isLoggedIn;
+    @Nullable
+    private boolean isLoggedIn;
 
     public Observable<Login> udate(){
         return Rxhelper;
     }
-
 
 }
